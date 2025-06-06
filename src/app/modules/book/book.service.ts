c6e87d64231c6c1514740b63c14eb3fb30e42a5f
mhlehri <lehrimirza101@gmail.com>
2025-06-06 16:57:38 +0600

    add book service with CRUD functions

---
+++ src/app/modules/book/book.service.ts
@@ -0,0 +1,13 @@
+const createBook = () => {};
+const readAllBooks = () => {};
+const readBookById = () => {};
+const updateBook = () => {};
+const deleteBook = () => {};
+
+export const bookService = {
+  createBook,
+  readAllBooks,
+  readBookById,
+  updateBook,
+  deleteBook,
+};
