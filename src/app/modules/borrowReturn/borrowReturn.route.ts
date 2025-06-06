f4fcf4c19660389d2a4b1aa58465485cfad34900
mhlehri <lehrimirza101@gmail.com>
2025-06-07 01:20:02 +0600

    fix: correct route handler for overdue books in borrowReturn.route.ts

--- src/app/modules/borrowReturn/borrowReturn.route.ts
+++ src/app/modules/borrowReturn/borrowReturn.route.ts
@@ -3,7 +3,10 @@ import { borrowReturnController } from "./borrowReturn.controller";
 
 const borrowReturnRouter = Router();
 
-borrowReturnRouter.get("/borrow/overdue", borrowReturnController.returnABook);
+borrowReturnRouter.get(
+  "/borrow/overdue",
+  borrowReturnController.borrowOverdueList
+);
 borrowReturnRouter.post("/borrow", borrowReturnController.borrowABook);
 borrowReturnRouter.post("/return", borrowReturnController.returnABook);
 
