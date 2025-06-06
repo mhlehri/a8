997e232683b76d492164a06406588bfeb898e94d
mhlehri <lehrimirza101@gmail.com>
2025-06-06 18:02:55 +0600

    fix: implement createBook method in book.controller.ts

--- src/app/modules/book/book.controller.ts
+++ src/app/modules/book/book.controller.ts
@@ -1,8 +1,20 @@
-const createBook = () => {};
-const readAllBooks = () => {};
-const readBookById = () => {};
-const updateBook = () => {};
-const deleteBook = () => {};
+import CatchAsync from "../../../helper/CatchAsync";
+import sendResponse from "../../../helper/sendRespose";
+import { bookService } from "./book.service";
+
+const createBook = CatchAsync((req, res) => {
+  const result = bookService.createBook(req.body);
+  sendResponse(res, {
+    success: true,
+    status: 201,
+    message: "Book created successfully",
+    data: result,
+  });
+});
+const readAllBooks = CatchAsync(() => {});
+const readBookById = CatchAsync(() => {});
+const updateBook = CatchAsync(() => {});
+const deleteBook = CatchAsync(() => {});
 
 export const bookController = {
   createBook,
