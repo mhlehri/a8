b7ef8bd330212a14c3573e0d4ae43690ee02f2c5
mhlehri <lehrimirza101@gmail.com>
2025-06-06 23:52:15 +0600

    fix: implement createMember method in member.controller.ts

--- src/app/modules/member/member.controller.ts
+++ src/app/modules/member/member.controller.ts
@@ -1,4 +1,18 @@
-const createMember = () => {};
+import CatchAsync from "../../../helper/CatchAsync";
+import sendResponse from "../../../helper/sendRespose";
+import { memberService } from "./member.service";
+
+const createMember = CatchAsync(async (req, res) => {
+  const result = await memberService.createMember(req.body);
+
+  sendResponse(res, {
+    success: true,
+    status: 201,
+    message: "Member created successfully",
+    data: result,
+  });
+});
+
 const readAllMembers = () => {};
 const readMemberById = () => {};
 const updateMember = () => {};
