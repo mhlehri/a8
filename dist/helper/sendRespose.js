3b6f7af503ee97c186bba7f32a0f8b744a52e586
mhlehri <lehrimirza101@gmail.com>
2025-06-07 03:58:38 +0600

    refactor: remove unused files and clean up the codebase

--- dist/helper/sendRespose.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sendResponse = (res, jsonData) => {
-    res.status(jsonData.status).json(jsonData);
-};
-exports.default = sendResponse;
--- dist/shared/prisma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generated_1 = require("../generated");
-const prisma = new generated_1.PrismaClient();
-exports.default = prisma;
