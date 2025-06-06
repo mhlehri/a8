59a3f630f75ee4e39c8cc14810fe08924964b9b0
mhlehri <lehrimirza101@gmail.com>
2025-06-06 16:59:52 +0600

    fix: associate appRouter with the /api route in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import cors from "cors";
 import express, { Application } from "express";
+import appRouter from "./app/routes";
 
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api");
+app.use("/api", appRouter);
 
 export default app;
