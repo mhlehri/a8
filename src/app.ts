7a3e5dcc931b0d9248e5f94f025c37d33d4091c6
mhlehri <lehrimirza101@gmail.com>
2025-06-06 17:01:48 +0600

    fix: add global and not found error handler middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import cors from "cors";
 import express, { Application } from "express";
 import appRouter from "./app/routes";
+import handleError from "./middleware/handleError";
+import handle404Error from "./middleware/handle404Error";
 
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", appRouter);
+// global error handler middleware
+app.use(handleError);
+// not found error handler middleware
+app.use(handle404Error);
 
 export default app;
