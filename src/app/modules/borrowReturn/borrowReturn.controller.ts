2152457b213f83ed64e9cb411a1c3ecbdf5007cf
mhlehri <lehrimirza101@gmail.com>
2025-06-07 00:33:07 +0600

    fix: implement borrowABook method in borrowReturn.controller.ts for borrowing books

--- src/app/modules/borrowReturn/borrowReturn.controller.ts
+++ src/app/modules/borrowReturn/borrowReturn.controller.ts
@@ -1,6 +1,17 @@
 import CatchAsync from "../../../helper/CatchAsync";
+import sendResponse from "../../../helper/sendRespose";
+import { borrowReturnService } from "./borrowReturn.service";
 
-const borrowABook = CatchAsync(async (req, res) => {});
+const borrowABook = CatchAsync(async (req, res) => {
+  const result = await borrowReturnService.borrowABook(req.body);
+
+  sendResponse(res, {
+    success: true,
+    status: 200,
+    message: "Book borrowed successfully",
+    data: result,
+  });
+});
 const returnABook = CatchAsync(async (req, res) => {});
 
 export const borrowReturnController = {
