dd1deed00911a232c442f57450396169864bc62f
mhlehri <lehrimirza101@gmail.com>
2025-06-06 16:55:41 +0600

    add book controller with CRUD functions

---
+++ src/app/modules/book/book.controller.ts
@@ -0,0 +1,13 @@
+const createBook = () => {};
+const readAllBooks = () => {};
+const readBookById = () => {};
+const updateBook = () => {};
+const deleteBook = () => {};
+
+export const bookController = {
+  createBook,
+  readAllBooks,
+  readBookById,
+  updateBook,
+  deleteBook,
+};
