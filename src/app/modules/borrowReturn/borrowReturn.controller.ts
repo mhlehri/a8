e2ab1f18710369e4a0fb51cc2b335037ca02751e
mhlehri <lehrimirza101@gmail.com>
2025-06-07 00:31:03 +0600

    fix: wrap borrowABook and returnABook methods with CatchAsync for error handling

--- src/app/modules/borrowReturn/borrowReturn.controller.ts
+++ src/app/modules/borrowReturn/borrowReturn.controller.ts
@@ -1,5 +1,7 @@
-const borrowABook = () => {};
-const returnABook = () => {};
+import CatchAsync from "../../../helper/CatchAsync";
+
+const borrowABook = CatchAsync(async (req, res) => {});
+const returnABook = CatchAsync(async (req, res) => {});
 
 export const borrowReturnController = {
   borrowABook,
