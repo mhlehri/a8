a158e5c611212de6eedf13b35861dbda50f00c37
mhlehri <lehrimirza101@gmail.com>
2025-06-07 00:50:26 +0600

    fix: implement borrowABook method in borrowReturn.service.ts for borrowing books with error handling

--- src/app/modules/borrowReturn/borrowReturn.service.ts
+++ src/app/modules/borrowReturn/borrowReturn.service.ts
@@ -1,5 +1,35 @@
-const borrowABook = () => {};
-const returnABook = () => {};
+import { BorrowRecord } from "../../../generated/prisma";
+import AppError from "../../../helper/AppError";
+import prisma from "../../../shared/prisma";
+
+const borrowABook = async (data: BorrowRecord) => {
+  const isBookExits = await prisma.book.findUnique({
+    where: {
+      bookId: data.bookId,
+    },
+  });
+
+  if (!isBookExits) {
+    throw new AppError(404, "Book doesn't exits");
+  }
+
+  const isMemberExits = await prisma.member.findUnique({
+    where: {
+      memberId: data.memberId,
+    },
+  });
+
+  if (!isMemberExits) {
+    throw new AppError(404, "Member doesn't exits");
+  }
+
+  const r = await prisma.borrowRecord.create({
+    data,
+  });
+
+  return r;
+};
+const returnABook = async () => {};
 
 export const borrowReturnService = {
   borrowABook,
