11e60183e8a169dd50409c7756ba20764fe2702d
mhlehri <lehrimirza101@gmail.com>
2025-06-06 17:07:58 +0600

    fix: update member validation schema to include name, email, phone, and membership date

--- src/app/modules/member/member.validation.ts
+++ src/app/modules/member/member.validation.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
 
 export const zodBookSchema = z.object({
-  title: z.string(),
-  genre: z.string(),
-  publishedYear: z.number(),
-  totalCopies: z.number(),
-  availableCopies: z.number(),
+  name: z.string(),
+  email: z.string(),
+  phone: z.string(),
+  membershipDate: z.date(),
 });
