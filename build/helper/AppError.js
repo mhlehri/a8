7dafc09a0840d6aca04a9da6ce61db106dbbbcc7
mhlehri <lehrimirza101@gmail.com>
2025-06-07 02:53:14 +0600

    refactor: remove unused build files and update .gitignore to exclude build directory

--- build/helper/AppError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AppError extends Error {
-    constructor(status, message) {
-        super(message);
-        this.status = status;
-    }
-}
-exports.default = AppError;
