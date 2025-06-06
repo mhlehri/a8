eb3d73727edf34e03f3519202ba9c637a367a102
mhlehri <lehrimirza101@gmail.com>
2025-06-07 01:08:51 +0600

    fix: update borrowABook method to filter out null values from response data

--- src/app/modules/borrowReturn/borrowReturn.controller.ts
+++ src/app/modules/borrowReturn/borrowReturn.controller.ts
@@ -5,11 +5,23 @@ import { borrowReturnService } from "./borrowReturn.service";
 const borrowABook = CatchAsync(async (req, res) => {
   const result = await borrowReturnService.borrowABook(req.body);
 
+  const resultData: Record<string, any> = {};
+  if (result) {
+    for (const key in result) {
+      if (Object.prototype.hasOwnProperty.call(result, key)) {
+        const typedKey = key as keyof typeof result;
+        if (result[typedKey] !== null) {
+          resultData[typedKey] = result[typedKey];
+        }
+      }
+    }
+  }
+
   sendResponse(res, {
     success: true,
     status: 200,
     message: "Book borrowed successfully",
-    data: result,
+    data: resultData,
   });
 });
 const returnABook = CatchAsync(async (req, res) => {});
